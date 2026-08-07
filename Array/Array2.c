f1283f15d12490e97cbdb2f0d184964c9296c316
Shubham Uikey <shubhamuikeys521@gmail.com>
2026-08-07 23:50:29 +0530

    some array problem

---
+++ Array/Array2.c
@@ -0,0 +1,28 @@
+Easy
+Problem 1: Sum of Elements
+
+Write a C program to input 10 integers into an array and print the sum of all elements.
+
+Example
+
+Input:
+2 4 6 8 10 12 14 16 18 20
+
+Output:
+Sum = 110
+Problem 2: Largest Element
+
+Write a program to find the largest element in an array of 10 integers.
+
+Problem 3: Count Even and Odd
+
+Write a program to count how many even and odd numbers are present in an array.
+
+Example
+
+// Input:
+// 1 2 3 4 5
+
+// Output:
+// Even = 2
+// Odd = 3
